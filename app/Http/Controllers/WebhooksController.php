c8719fbcb0ad4d54332115e8ee2d7beec4e9eb25
Michael Heap <m@michaelheap.com>
2018-12-12 16:57:14 +0000

    Send notifications in response to inbound message.
    Install the nexmo/laravel-notification package to get access to the
    Message API notification channels.
    
    When a user sends a message, try and find a recipe with the same name.
    If it exists, loop through all of the steps required to make that meal
    and schedule them as individual notifications, delayed as appropriate.

--- app/Http/Controllers/WebhooksController.php
+++ app/Http/Controllers/WebhooksController.php
@@ -3,11 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Notification;
+
+use App\Notifications\FreeText;
 
 class WebhooksController extends Controller
 {
     public function inboundMessage(Request $request) {
         \Log::debug('Inbound Message', $request->all());
+
+        // The incoming message contains the platform + contact details that
+        // we need to reply with, so configure a notification route with those
+        // details
+        $from = $request->input('from');
+        $channel = 'nexmo-' . $from['type'];
+        $sender = Notification::route($channel, $from['id']);
+
+        // Try and find the recipe name that was sent to us
+        $recipeName = $request->input('message.content.text');
+        $recipe = \App\Recipe::where('name', $recipeName)->first();
+        if (!$recipe) {
+            $sender->notify(new FreeText(
+                "I couldn't find that recipe",
+                $channel
+            ));
+            return;
+        }
+
+        // If we get this far, we have a recipe! Time to schedule some notifications
+        // We use ->delay() to delay the notification by X seconds, which is exactly
+        // what we need to do to make the messages appear when action should be taken
+        foreach ($recipe->timings()->get() as $t) {
+            $sender->notify((new FreeText(
+                $t->action,
+                $channel
+            ))->delay($t->start_time));
+        }
     }
 
     public function messageStatus(Request $request) {
